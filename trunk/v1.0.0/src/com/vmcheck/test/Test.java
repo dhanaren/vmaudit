162d5c38bc2a8bbc16744e0e5a3a03c3a0bd94bf
dhanaren <dhanaren@cdfe4376-76d9-a08b-f58b-97147bf07926>
2011-04-22 20:53:09 +0000

    2342011 dhanaren_create child branch

---
+++ trunk/v1.0.0/src/com/vmcheck/test/Test.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.vmcheck.test;
+
+/**
+ * @author Dhanasekar
+ *
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world...");
+	}
+
+}
